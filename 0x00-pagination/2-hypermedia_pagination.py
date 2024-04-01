b876465ad8f7b4b1d4f28abb94adab0354401506
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-04-01 21:35:52 +0000

    End of 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -3,7 +3,7 @@
 """
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 def index_range(page, page_size):
@@ -52,3 +52,19 @@ class Server:
             return []
         else:
             return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        assert isinstance(page, int) and page > 0, "page must be \
+                an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "page_size \
+                must be an integer greater than 0"
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
