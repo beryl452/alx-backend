f75154faa52fa8b7bbdc9c54f520e73b426374d4
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-04-03 10:14:43 +0000

    End of 100-lfu_cache.py

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""5. LFU Caching
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    _frequency = {}
+
+    def __init__(self):
+        """
+        Initializes an instance of MRUCache.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache.
+
+        Args:
+            key (hashable): The key to associate with the item.
+            item: The item to be added to the cache.
+
+        Returns:
+            None
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data.keys():
+                min_freq = min(self._frequency.values())
+                keys = [k for k, v in self._frequency.items() if v == min_freq]
+                if len(keys) == 1:
+                    del self.cache_data[keys[0]]
+                    del self._frequency[keys[0]]
+                else:
+                    min_key = None
+                    for k in self.cache_data.keys():
+                        if min_key is None and k in keys:
+                            min_key = k
+                            break
+                    if min_key:
+                        del self.cache_data[min_key]
+                        del self._frequency[min_key]
+                        print(f"DISCARD: {min_key}")
+            if key in self.cache_data.keys():
+                del self.cache_data[key]
+                self._frequency[key] += 1
+            else:
+                self._frequency[key] = 1
+            self.cache_data[key] = item
+        
+
+    def get(self, key):
+        """
+        Retrieves the value associated with the given key from the cache.
+
+        Args:
+            key (hashable): The key to look up in the cache.
+
+        Returns:
+            The value associated with the key,
+            or None if the key is not found in the cache.
+        """
+        if key in self.cache_data.keys():
+            self._frequency[key] += 1
+            value = self.cache_data[key]
+            del self.cache_data[key]
+            self.cache_data[key] = value
+            return value
+        return None       
