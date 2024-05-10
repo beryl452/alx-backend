1e0250301b54030ed90918f65b5893df2b517fde
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-05-10 04:21:31 -0700

    End of 2. Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,6 +1,8 @@
 import { createClient, print } from 'redis';
+import { promisify } from 'util';
 
 const client = createClient();
+const getAsync = promisify(client.get).bind(client);
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
@@ -14,14 +16,13 @@ function setNewSchool(schoolName, value) {
   client.set(schoolName, value, print);
 }
 
-function displaySchoolValue(schoolName) {
-  client.get(schoolName, (error, result) => {
-    if (error) {
-      console.log(error);
-      throw error;
-   }
-   console.log(result);
-  });
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 displaySchoolValue('Holberton');
