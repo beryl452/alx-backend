b48ef5c1e9d1f8236778c987253cb46f2d252c2f
Béryl HOUESSOU <berylhoe@gmail.com>
2024-07-10 16:36:40 +0100

    11. Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,33 +1,32 @@
-import kue from 'kue';
+import { Queue, Job } from 'kue';
 
 /**
- * Crée des tâches de notification push dans la file d'attente.
- * @param {Object[]} jobs - Un tableau d'objets représentant les tâches à créer.
- * @param {kue.Queue} queue - La file d'attente Kue.
+ * Creates push notification jobs from the array of jobs info.
+ * @param {Job[]} jobs
+ * @param {Queue} queue
  */
-function createPushNotificationsJobs(jobs, queue) {
-  if (!Array.isArray(jobs)) {
+export const createPushNotificationsJobs = (jobs, queue) => {
+  if (!(jobs instanceof Array)) {
     throw new Error('Jobs is not an array');
   }
+  for (const jobInfo of jobs) {
+    const job = queue.create('push_notification_code_3', jobInfo);
 
-  jobs.forEach((jobData) => {
-    const job = queue.create('push_notification_code_3', jobData)
-      .save((err) => {
-        if (err) {
-          console.log(`Failed to create job: ${err}`);
-        } else {
-          console.log(`Notification job created: ${job.id}`);
-        }
+    job
+      .on('enqueue', () => {
+        console.log('Notification job created:', job.id);
+      })
+      .on('complete', () => {
+        console.log('Notification job', job.id, 'completed');
+      })
+      .on('failed', (err) => {
+        console.log('Notification job', job.id, 'failed:', err.message || err.toString());
+      })
+      .on('progress', (progress, _data) => {
+        console.log('Notification job', job.id, `${progress}% complete`);
       });
-
-    job.on('complete', () => {
-      console.log(`Notification job ${job.id} completed`);
-    }).on('failed', (err) => {
-      console.log(`Notification job ${job.id} failed: ${err}`);
-    }).on('progress', (progress) => {
-      console.log(`Notification job ${job.id} ${progress}% complete`);
-    });
-  });
-}
+    job.save();
+  }
+};
 
 export default createPushNotificationsJobs;
